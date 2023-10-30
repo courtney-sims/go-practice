6e01180ab2f0e1cc57b1b8d4156b5cdd1dd80a58
Courtney Sims <courtney.sims@wpengine.com>
2023-10-30 15:54:43 -0500

    refactor

--- manyworlds/hello_test.go
+++ manyworlds/hello_test.go
@@ -7,17 +7,19 @@ func TestHello(t *testing.T) {
 		got := Hello("Bubble")
 		want := "Hello, Bubble World."
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("handle empty world string", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, New World."
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
