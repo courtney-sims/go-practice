1c2a41c027a5e7eaf96b1e7a4bded33c42754abb
Courtney Sims <courtney.sims@wpengine.com>
2023-10-30 12:49:34 -0500

    customize world name

--- bubbleworld/hello.go
+++ bubbleworld/hello.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello, Bubble World."
+func Hello(worldName string) string {
+	return "Hello, " + worldName + " World."
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("some"))
 }
